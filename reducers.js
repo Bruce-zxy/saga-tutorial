5e902cec6689f37af6fca8ef3d2e52caa860c672
bruce-zxy <bruce_zxy@163.com>
2018-01-31 23:25:26 +0800

    increase number after one second only (step by step)

--- reducers.js
+++ reducers.js
@@ -1,19 +1,32 @@
 export default function counter(state = {
-    count: 0
+    count: 0,
+    inRunning: false
 }, action) {
-    console.log(state);
     switch (action.type) {
         case 'INCREMENT':
             return {
-                count: state.count + 1
+                count: state.count + 1,
+                inRunning: false
             }
         case 'INCREMENT_IF_ODD':
             return {
-                count: (state.count % 2 !== 0) ? state.count + 1 : state.count
+                count: (state.count % 2 !== 0) ? state.count + 1 : state.count,
+                inRunning: false
             }
         case 'DECREMENT':
             return {
-                count: state.count - 1
+                count: state.count - 1,
+                inRunning: false
+            }
+        case 'START':
+            return {
+                count: state.count,
+                inRunning: true
+            }
+        case 'END':
+            return {
+                count: state.count,
+                inRunning: false
             }
         default:
             return state
--- sagas.js
+++ sagas.js
@@ -1,14 +1,23 @@
 import { delay } from 'redux-saga'
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, select, takeEvery, all } from 'redux-saga/effects'
 
 // 一个工具函数：返回一个 Promise，这个 Promise 将在 1 秒后 resolve
 // export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 // Our worker Saga: 将异步执行 increment 任务
 export function* incrementAsync() {
+    if(yield select(state => state.inRunning)) {
+    	return false;
+    }
+	yield put({
+        type: 'START'
+    })
     yield delay(1000)
     yield put({
         type: 'INCREMENT'
+    })
+    yield put({
+        type: 'END'
     })
 }
 
