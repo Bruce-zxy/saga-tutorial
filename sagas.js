a8e76ca9ed3007a2c0047ea6cfbf71c923e7ce31
bruce-zxy <bruce_zxy@163.com>
2018-02-01 18:22:26 +0800

    run test for check code

--- sagas.js
+++ sagas.js
@@ -1,38 +1,37 @@
-import { delay } from 'redux-saga'
-import { put, select, takeEvery, all } from 'redux-saga/effects'
+// import { delay } from 'redux-saga'
+import { put, select, takeEvery, all, call } from 'redux-saga/effects'
 
 // 一个工具函数：返回一个 Promise，这个 Promise 将在 1 秒后 resolve
-// export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+export const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 // Our worker Saga: 将异步执行 increment 任务
 export function* incrementAsync() {
-    if(yield select(state => state.inRunning)) {
-    	return false;
-    }
+    if(yield select(state => state.inRunning)) return false;
 	yield put({
         type: 'START'
-    })
-    yield delay(1000)
+    });
+    yield call(delay, 1000);
     yield put({
         type: 'INCREMENT'
-    })
+    });
     yield put({
         type: 'END'
-    })
-}
+    });
+};
 
 // Our watcher Saga: 在每个 INCREMENT_ASYNC action 调用后，派生一个新的 incrementAsync 任务
 export function* watchIncrementAsync() {
-    yield takeEvery('INCREMENT_ASYNC', incrementAsync)
-}
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync);
+};
 
 export function* helloSaga() {
-    console.log('Hello Sagas!');
-}
+    yield console.log('Hello Sagas!');
+    yield console.log('Hello Yield!');
+};
 
 export default function* rootSaga() {
     yield all([
         helloSaga(),
         watchIncrementAsync()
-    ])
-}
+    ]);
+};
